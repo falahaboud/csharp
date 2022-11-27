563dd732a46800b79452ff1d6f74d9ec34ca3c6e
falahaboud <falah.aboud@gmail.com>
2022-11-27 22:20:11 +0100

    first commit

---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿using Test;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
